657f4972dc97796ba3d3b7517d698471585c63e0
糖饼 <sugarpie.tang@gmail.com>
2017-05-01 20:16:43 +0800

    利用htmlResourceRules来解析资源

--- src/index.js
+++ src/index.js
@@ -1,112 +1,83 @@
-const template = require('art-template/lib/template-node');
-const precompile = require('art-template/lib/precompile');
 const loaderUtils = require('loader-utils');
-const attrParse = require('./attributes-parser');
-const url = require('url');
-const randomIdent = () => {
-	return 'xxxHTMLLINKxxx' + Math.random() + Math.random() + 'xxx';
-}
+const template = require('art-template');
+const precompile = require('art-template/lib/precompile');
 
-const openTag = `\x02\x02`;
-const closeTag = `\x03\x03`;
-template.defaults.rules.unshift({
-	test: new RegExp(openTag + '(.*?)' + closeTag),
-	use: (match, code) => {
-		return {
-			output: 'raw',
-			code: `require(${JSON.stringify(code)})`
-		}
-	}
-});
+// Proprietary options: htmlResourceRoot, htmlResourceRules
 
-const loader = function(source) {
-
-	this.cacheable && this.cacheable();
+const loader = function (source) {
 
 	let result;
-	let attributes = ['img:src'];
-    // htmlAttrs, htmlResourceRoot
-	let options = loaderUtils.getOptions(this) || {};
 	const callback = this.callback;
+	let options = loaderUtils.getOptions(this) || {};
+	let htmlResourceRules = [/\bsrc="([^"]*)"/];
+	const htmlResourceRoot = options.htmlResourceRoot;
+	const use = (match, url) => {
+		let code;
+		const output = 'raw';
 
-	if(options.debug === undefined) {
+		if (loaderUtils.isUrlRequest(url, htmlResourceRoot)) {
+			const urlRequest = loaderUtils.urlToRequest(url, htmlResourceRoot);
+			const attr = match.split(url);
+			const codes = [attr[0], urlRequest, attr[1]].map(JSON.stringify);
+			code = codes[0] + `+require(${codes[1]})+` + codes[2];
+		} else {
+			code = match;
+		}
+
+		return {
+			output,
+			code
+		};
+	};
+
+
+	if (this.cacheable) {
+		this.cacheable();
+	}
+
+	if (options.debug === undefined) {
 		options.debug = this.debug;
 	}
 
-	if(options.minimize === undefined) {
+	if (options.minimize === undefined) {
 		options.minimize = this.minimize;
 	}
 
-	if(options.htmlAttrs !== undefined) {
-		if(typeof options.htmlAttrs === `string`) {
-			attributes = options.htmlAttrs.split(` `);
-		} else if(Array.isArray(options.htmlAttrs)) {
-			attributes = options.htmlAttrs;
-		} else if(options.htmlAttrs === false) {
-			attributes = [];
+	if (options.htmlResourceRules !== undefined) {
+		if (Array.isArray(options.htmlResourceRules)) {
+			htmlResourceRules = options.htmlResourceRules;
+		} else if (options.htmlResourceRules === false) {
+			htmlResourceRules = [];
 		} else {
-			throw new Error(`Invalid value to options parameter htmlAttrs`);
+			throw new Error(`Invalid value to options parameter htmlResourceRules`);
 		}
 	}
 
-	options = template.defaults.$extend(options);
-	const htmlResourceRoot = options.htmlResourceRoot;
-	const links = attrParse(source, function(tag, attr) {
-		return attributes.indexOf(tag + ":" + attr) >= 0;
-	});
-
-	links.reverse();
-
-	const data = {};
-	source = [source];
-	links.forEach(function(link) {
-		if(!loaderUtils.isUrlRequest(link.value, htmlResourceRoot)) {
-			return
-		};
-
-		let ident;
-		const uri = url.parse(link.value);
-
-		if(uri.hash !== null && uri.hash !== undefined) {
-			uri.hash = null;
-			link.value = uri.format();
-			link.length = link.value.length;
-		}
-
-		do {
-			ident = randomIdent();
-		} while (data[ident]);
-
-		data[ident] = link.value;
-		const x = source.pop();
-		source.push(x.substr(link.start + link.length));
-		source.push(ident);
-		source.push(x.substr(0, link.start));
-	});
-	source.reverse();
-	source = source.join(``);
-	source = source.replace(/xxxHTMLLINKxxx[0-9\.]+xxx/g, match => {
-		if(!data[match]) {
-			return match
-		};
-		return openTag + loaderUtils.urlToRequest(data[match], htmlResourceRoot) + closeTag;
-	});
-
-	// use mocha
-	if(loader.__returnString) {
-		return source;
+	if (options.rules === undefined) {
+		options.rules = [];
 	}
+
+	if (options.ignore === undefined) {
+		options.ignore = [];
+	}
+
+	options.rules.push(...template.defaults.rules);
+	htmlResourceRules.forEach(test => {
+		options.rules.push({
+			test,
+			use
+		});
+	});
 
 	options.source = source;
 	options.filename = this.resourcePath;
 	options.sourceMap = this.sourceMap;
 	options.sourceRoot = process.cwd();
-	options.ignore = options.ignore || [];
 	options.ignore.push(`require`);
 
 	try {
 		result = precompile(options);
-	} catch(error) {
+	} catch (error) {
 		delete error.stack; // 这样才能打印 art-template 调试信息
 		callback(error);
 		return;
@@ -116,11 +87,13 @@ const loader = function(source) {
 	const sourceMap = result.sourceMap;
 	const ast = result.ast;
 
-	if(sourceMap && (!sourceMap.sourcesContent || !sourceMap.sourcesContent.length)) {
+	if (sourceMap && (!sourceMap.sourcesContent || !sourceMap.sourcesContent.length)) {
 		sourceMap.sourcesContent = [source];
 	}
 
 	callback(null, code, sourceMap, ast);
 };
+
+
 
 module.exports = loader;
